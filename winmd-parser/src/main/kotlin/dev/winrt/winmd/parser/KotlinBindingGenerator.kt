c19f1f175cb9f6c8fc452cf926e456ae44bd4523
sanlorng <fujia625@gmail.com>
2026-03-25 14:45:58 +0800

    Extract type file emitter

--- winmd-parser/src/main/kotlin/dev/winrt/winmd/parser/KotlinBindingGenerator.kt
+++ winmd-parser/src/main/kotlin/dev/winrt/winmd/parser/KotlinBindingGenerator.kt
@@ -1,48 +1,13 @@
 package dev.winrt.winmd.parser
 
-import com.squareup.kotlinpoet.FileSpec
 import dev.winrt.winmd.plugin.WinMdModel
-import dev.winrt.winmd.plugin.WinMdNamespace
-import dev.winrt.winmd.plugin.WinMdType
-import dev.winrt.winmd.plugin.WinMdTypeKind
 
 class KotlinBindingGenerator {
-    private val typeNameMapper = TypeNameMapper()
-    private val interfaceTypeRenderer = InterfaceTypeRenderer(typeNameMapper)
-    private val runtimePropertyRenderer = RuntimePropertyRenderer(typeNameMapper)
-    private val runtimeMethodRenderer = RuntimeMethodRenderer(typeNameMapper)
-    private val runtimeCompanionRenderer = RuntimeCompanionRenderer()
-    private val runtimeProjectionRenderer = RuntimeProjectionRenderer()
-    private val valueTypeRenderer = ValueTypeRenderer(typeNameMapper)
-    private val runtimeTypeRenderer = RuntimeTypeRenderer(
-        runtimePropertyRenderer,
-        runtimeMethodRenderer,
-        runtimeCompanionRenderer,
-        runtimeProjectionRenderer,
-    )
+    private val typeFileEmitter = TypeFileEmitter()
 
     fun generate(model: WinMdModel): List<GeneratedFile> {
         return model.namespaces.flatMap { namespace ->
-            namespace.types.map { type ->
-                GeneratedFile(
-                    relativePath = namespace.name.replace('.', '/') + "/${type.name}.kt",
-                    content = renderTypeFile(namespace, type).toString(),
-                )
-            }
+            namespace.types.map { type -> typeFileEmitter.emit(namespace, type) }
         }
-    }
-
-    private fun renderTypeFile(namespace: WinMdNamespace, type: WinMdType): FileSpec {
-        return FileSpec.builder(namespace.name.lowercase(), type.name)
-            .addType(renderType(type))
-            .build()
-    }
-
-    private fun renderType(type: WinMdType) = when (type.kind) {
-        WinMdTypeKind.Interface -> interfaceTypeRenderer.render(type)
-        WinMdTypeKind.RuntimeClass -> runtimeTypeRenderer.render(type)
-        WinMdTypeKind.Struct,
-        WinMdTypeKind.Enum,
-        -> valueTypeRenderer.render(type)
     }
 }
