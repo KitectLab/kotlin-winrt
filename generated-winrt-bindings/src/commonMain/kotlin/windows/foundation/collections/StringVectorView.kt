5d25e7891a96890eb6e70aeb1d61d2ba1528db64
sanlorng <fujia625@gmail.com>
2026-03-29 18:39:08 +0800

    Implement StringVectorView as a WinRtObject class

--- generated-winrt-bindings/src/commonMain/kotlin/windows/foundation/collections/StringVectorView.kt
+++ generated-winrt-bindings/src/commonMain/kotlin/windows/foundation/collections/StringVectorView.kt
@@ -10,7 +10,8 @@ import kotlin.io.use
 public open class StringVectorView(
   pointer: ComPtr,
 ) : WinRtObject(pointer),
-  List<String> by createListDelegate(pointer) {
+  List<String> {
+  private val listDelegate: List<String> = createListDelegate(pointer)
 
   public val winRtSize: UInt32
     get() = UInt32(PlatformComInterop.invokeUInt32Method(pointer, 7).getOrThrow())
@@ -20,6 +21,29 @@ public open class StringVectorView(
       it.toKotlinString()
     }
   }
+
+  override val size: Int
+    get() = listDelegate.size
+
+  override fun contains(element: String): Boolean = listDelegate.contains(element)
+
+  override fun containsAll(elements: Collection<String>): Boolean = listDelegate.containsAll(elements)
+
+  override fun get(index: Int): String = listDelegate[index]
+
+  override fun indexOf(element: String): Int = listDelegate.indexOf(element)
+
+  override fun isEmpty(): Boolean = listDelegate.isEmpty()
+
+  override fun iterator(): Iterator<String> = listDelegate.iterator()
+
+  override fun lastIndexOf(element: String): Int = listDelegate.lastIndexOf(element)
+
+  override fun listIterator(): ListIterator<String> = listDelegate.listIterator()
+
+  override fun listIterator(index: Int): ListIterator<String> = listDelegate.listIterator(index)
+
+  override fun subList(fromIndex: Int, toIndex: Int): List<String> = listDelegate.subList(fromIndex, toIndex)
 
   public companion object {
     private fun createListDelegate(pointer: ComPtr): List<String> =
