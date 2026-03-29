b00ae8eda2083faa6769214c38bd358df36fb71e
sanlorng <fujia625@gmail.com>
2026-03-29 17:54:42 +0800

    Make HString AutoCloseable

--- kom/src/commonMain/kotlin/dev/winrt/kom/HString.kt
+++ kom/src/commonMain/kotlin/dev/winrt/kom/HString.kt
@@ -3,13 +3,17 @@ package dev.winrt.kom
 import kotlin.jvm.JvmInline
 
 @JvmInline
-value class HString(val raw: Long) {
+value class HString(val raw: Long) : AutoCloseable {
     companion object {
         val NULL = HString(0)
     }
 
     val isNull: Boolean
         get() = raw == 0L
+
+    override fun close() {
+        PlatformHStringBridge.release(this)
+    }
 }
 
 interface HStringBridge {
