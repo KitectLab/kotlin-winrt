cd211c568b8a58820e7e559ecefa04a2e572613e
sanlorng <fujia625@gmail.com>
2026-03-29 17:05:42 +0800

    Project Json interfaces as interfaces

--- generated-winrt-bindings/src/commonMain/kotlin/windows/data/json/IJsonObject.kt
+++ generated-winrt-bindings/src/commonMain/kotlin/windows/data/json/IJsonObject.kt
@@ -4,7 +4,6 @@ import dev.winrt.core.Float64
 import dev.winrt.core.Inspectable
 import dev.winrt.core.WinRtBoolean
 import dev.winrt.core.WinRtInterfaceMetadata
-import dev.winrt.core.WinRtInterfaceProjection
 import dev.winrt.core.WinRtStrings
 import dev.winrt.core.guidOf
 import dev.winrt.core.projectInterface
@@ -13,13 +12,37 @@ import dev.winrt.kom.Guid
 import dev.winrt.kom.PlatformComInterop
 import kotlin.String
 
-public open class IJsonObject(
-  pointer: ComPtr,
-) : WinRtInterfaceProjection(pointer) {
-  public fun getNamedValue(name: String): IJsonValue = IJsonValue(
-      PlatformComInterop.invokeObjectMethodWithStringArg(pointer, 6, name).getOrThrow())
+public interface IJsonObject {
+  public fun getNamedValue(name: String): IJsonValue
 
-  public fun getNamedString(name: String): String {
+  public fun getNamedString(name: String): String
+
+  public fun getNamedObject(name: String): JsonObject
+
+  public fun getNamedArray(name: String): JsonArray
+
+  public fun getNamedNumber(name: String): Float64
+
+  public fun getNamedBoolean(name: String): WinRtBoolean
+
+  public companion object : WinRtInterfaceMetadata {
+    override val qualifiedName: String = "Windows.Data.Json.IJsonObject"
+
+    override val iid: Guid = guidOf("064e24dd-29c2-4f83-9ac1-9ee11578beb3")
+
+    public fun from(inspectable: Inspectable): IJsonObject = inspectable.projectInterface(this,
+        ::IJsonObjectImpl)
+  }
+}
+
+private class IJsonObjectImpl(
+  pointer: ComPtr,
+) : dev.winrt.core.WinRtInterfaceProjection(pointer), IJsonObject {
+  override fun getNamedValue(name: String): IJsonValue = IJsonValue.from(
+      Inspectable(PlatformComInterop.invokeObjectMethodWithStringArg(pointer, 6, name)
+          .getOrThrow()))
+
+  override fun getNamedString(name: String): String {
     val value = PlatformComInterop.invokeHStringMethodWithStringArg(pointer, 10, name).getOrThrow()
     return try {
       WinRtStrings.toKotlin(value)
@@ -28,25 +51,16 @@ public open class IJsonObject(
     }
   }
 
-  public fun getNamedObject(name: String): JsonObject = JsonObject(
+  override fun getNamedObject(name: String): JsonObject = JsonObject(
       PlatformComInterop.invokeObjectMethodWithStringArg(pointer, 8, name).getOrThrow())
 
-  public fun getNamedArray(name: String): JsonArray = JsonArray(
+  override fun getNamedArray(name: String): JsonArray = JsonArray(
       PlatformComInterop.invokeObjectMethodWithStringArg(pointer, 9, name).getOrThrow())
 
-  public fun getNamedNumber(name: String): Float64 =
+  override fun getNamedNumber(name: String): Float64 =
       Float64(PlatformComInterop.invokeFloat64MethodWithStringArg(pointer, 11, name).getOrThrow())
 
-  public fun getNamedBoolean(name: String): WinRtBoolean =
+  override fun getNamedBoolean(name: String): WinRtBoolean =
       WinRtBoolean(PlatformComInterop.invokeBooleanMethodWithStringArg(pointer, 12,
       name).getOrThrow())
-
-  public companion object : WinRtInterfaceMetadata {
-    override val qualifiedName: String = "Windows.Data.Json.IJsonObject"
-
-    override val iid: Guid = guidOf("064e24dd-29c2-4f83-9ac1-9ee11578beb3")
-
-    public fun from(inspectable: Inspectable): IJsonObject = inspectable.projectInterface(this,
-        ::IJsonObject)
-  }
 }
--- generated-winrt-bindings/src/commonMain/kotlin/windows/data/json/IJsonValue.kt
+++ generated-winrt-bindings/src/commonMain/kotlin/windows/data/json/IJsonValue.kt
@@ -1,11 +1,10 @@
 package windows.`data`.json
 
-import dev.winrt.core.Inspectable
 import dev.winrt.core.Float64
+import dev.winrt.core.Inspectable
 import dev.winrt.core.WinRtBoolean
-import dev.winrt.core.WinRtStrings
 import dev.winrt.core.WinRtInterfaceMetadata
-import dev.winrt.core.WinRtInterfaceProjection
+import dev.winrt.core.WinRtStrings
 import dev.winrt.core.guidOf
 import dev.winrt.core.projectInterface
 import dev.winrt.kom.ComPtr
@@ -13,16 +12,43 @@ import dev.winrt.kom.Guid
 import dev.winrt.kom.PlatformComInterop
 import kotlin.String
 
-public open class IJsonValue(
-  pointer: ComPtr,
-) : WinRtInterfaceProjection(pointer) {
+public interface IJsonValue {
   public val valueType: JsonValueType
+
+  public fun get_ValueType(): JsonValueType
+
+  public fun stringify(): String
+
+  public fun getString(): String
+
+  public fun getNumber(): Float64
+
+  public fun getBoolean(): WinRtBoolean
+
+  public fun getArray(): JsonArray
+
+  public fun getObject(): JsonObject
+
+  public companion object : WinRtInterfaceMetadata {
+    override val qualifiedName: String = "Windows.Data.Json.IJsonValue"
+
+    override val iid: Guid = guidOf("a3219ecb-f0b3-4dcd-beee-19d48cd3ed1e")
+
+    public fun from(inspectable: Inspectable): IJsonValue = inspectable.projectInterface(this,
+        ::IJsonValueImpl)
+  }
+}
+
+private class IJsonValueImpl(
+  pointer: ComPtr,
+) : dev.winrt.core.WinRtInterfaceProjection(pointer), IJsonValue {
+  override val valueType: JsonValueType
     get() = get_ValueType()
 
-  public fun get_ValueType(): JsonValueType =
+  override fun get_ValueType(): JsonValueType =
       JsonValueType.fromValue(PlatformComInterop.invokeUInt32Method(pointer, 6).getOrThrow().toInt())
 
-  public fun stringify(): String {
+  override fun stringify(): String {
     val value = PlatformComInterop.invokeHStringMethod(pointer, 7).getOrThrow()
     return try {
       WinRtStrings.toKotlin(value)
@@ -31,7 +57,7 @@ public open class IJsonValue(
     }
   }
 
-  public fun getString(): String {
+  override fun getString(): String {
     val value = PlatformComInterop.invokeHStringMethod(pointer, 8).getOrThrow()
     return try {
       WinRtStrings.toKotlin(value)
@@ -40,24 +66,15 @@ public open class IJsonValue(
     }
   }
 
-  public fun getNumber(): Float64 =
+  override fun getNumber(): Float64 =
       Float64(PlatformComInterop.invokeFloat64Method(pointer, 9).getOrThrow())
 
-  public fun getBoolean(): WinRtBoolean =
+  override fun getBoolean(): WinRtBoolean =
       WinRtBoolean(PlatformComInterop.invokeBooleanGetter(pointer, 10).getOrThrow())
 
-  public fun getArray(): JsonArray = JsonArray(PlatformComInterop.invokeObjectMethod(pointer, 11)
+  override fun getArray(): JsonArray = JsonArray(PlatformComInterop.invokeObjectMethod(pointer, 11)
       .getOrThrow())
 
-  public fun getObject(): JsonObject = JsonObject(PlatformComInterop.invokeObjectMethod(pointer,
+  override fun getObject(): JsonObject = JsonObject(PlatformComInterop.invokeObjectMethod(pointer,
       12).getOrThrow())
-
-  public companion object : WinRtInterfaceMetadata {
-    override val qualifiedName: String = "Windows.Data.Json.IJsonValue"
-
-    override val iid: Guid = guidOf("a3219ecb-f0b3-4dcd-beee-19d48cd3ed1e")
-
-    public fun from(inspectable: Inspectable): IJsonValue = inspectable.projectInterface(this,
-        ::IJsonValue)
-  }
 }
