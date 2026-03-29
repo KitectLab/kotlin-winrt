67d493a67bed6277e5561bb371d35516f0436949
sanlorng <fujia625@gmail.com>
2026-03-29 19:10:23 +0800

    Add projection type mapper tests

---
+++ winmd-parser/src/test/kotlin/dev/winrt/winmd/parser/WinRtProjectionTypeMapperTest.kt
@@ -0,0 +1,38 @@
+package dev.winrt.winmd.parser
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class WinRtProjectionTypeMapperTest {
+    private val mapper = WinRtProjectionTypeMapper()
+
+    @Test
+    fun maps_runtime_class_projection_key_to_identity_by_default() {
+        assertEquals(
+            "Microsoft.UI.Xaml.UIElement",
+            mapper.projectionTypeKeyFor("Microsoft.UI.Xaml.UIElement", "Microsoft.UI.Xaml"),
+        )
+    }
+
+    @Test
+    fun maps_generic_collection_projection_key_with_nested_runtime_class_argument() {
+        assertEquals(
+            "System.Collections.Generic.IList<Microsoft.UI.Xaml.UIElement>",
+            mapper.projectionTypeKeyFor(
+                "Windows.Foundation.Collections.IVector`1<Microsoft.UI.Xaml.UIElement>",
+                "Microsoft.UI.Xaml.Controls",
+            ),
+        )
+    }
+
+    @Test
+    fun preserves_scalar_arguments_in_generic_projection_keys() {
+        assertEquals(
+            "System.Collections.Generic.IReadOnlyList<String>",
+            mapper.projectionTypeKeyFor(
+                "Windows.Foundation.Collections.IVectorView`1<String>",
+                "Windows.Foundation.Collections",
+            ),
+        )
+    }
+}
