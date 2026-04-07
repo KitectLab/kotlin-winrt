9f2f1e91e0e46add755583907fa0f8a63a364fef
sanlorng <fujia625@gmail.com>
2026-04-07 14:20:47 +0800

    Refactor duplicated WinRT delegate support

--- winrt-core/src/jvmMain/kotlin/dev/winrt/core/JvmWinRtBooleanResultDelegates.kt
+++ winrt-core/src/jvmMain/kotlin/dev/winrt/core/JvmWinRtBooleanResultDelegates.kt
@@ -7,25 +7,22 @@ import dev.winrt.kom.HResult
 import dev.winrt.kom.HString
 import dev.winrt.kom.KnownHResults
 import dev.winrt.kom.PlatformHStringBridge
-import java.lang.foreign.Arena
 import java.lang.foreign.FunctionDescriptor
-import java.lang.foreign.Linker
 import java.lang.foreign.MemorySegment
 import java.lang.foreign.ValueLayout
 import java.lang.invoke.MethodHandles
 import java.lang.invoke.MethodType
-import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.atomic.AtomicInteger
-import java.util.concurrent.atomic.AtomicBoolean
 
-internal object JvmWinRtBooleanResultDelegates {
+internal object JvmWinRtBooleanResultDelegates : JvmWinRtDelegateSupport() {
+    private val lookup = MethodHandles.lookup()
+
     fun createDelegate(
         iid: Guid,
         parameterKinds: List<WinRtDelegateValueKind>,
         invoke: (Array<Any?>) -> Boolean,
     ): WinRtDelegateHandle {
         val invoker = object : BooleanInvoker {
-            override fun invoke(state: State, args: Array<out Any?>): Boolean {
+            override fun invoke(args: Array<out Any?>): Boolean {
                 return invoke(decodeArguments(parameterKinds, args))
             }
         }
@@ -44,7 +41,7 @@ internal object JvmWinRtBooleanResultDelegates {
             methodType = MethodType.methodType(Int::class.javaPrimitiveType, MemorySegment::class.java, MemorySegment::class.java),
             descriptor = FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS, ValueLayout.ADDRESS),
             invoker = object : BooleanInvoker {
-                override fun invoke(state: State, args: Array<out Any?>): Boolean = callback()
+                override fun invoke(args: Array<out Any?>): Boolean = callback()
             },
         )
     }
@@ -55,7 +52,7 @@ internal object JvmWinRtBooleanResultDelegates {
             methodType = MethodType.methodType(Int::class.javaPrimitiveType, MemorySegment::class.java, Int::class.javaObjectType, MemorySegment::class.java),
             descriptor = FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS, ValueLayout.JAVA_INT, ValueLayout.ADDRESS),
             invoker = object : BooleanInvoker {
-                override fun invoke(state: State, args: Array<out Any?>): Boolean = invoke(decode(args[0] as Int))
+                override fun invoke(args: Array<out Any?>): Boolean = invoke(decode(args[0] as Int))
             },
         )
     }
@@ -66,7 +63,7 @@ internal object JvmWinRtBooleanResultDelegates {
             methodType = MethodType.methodType(Int::class.javaPrimitiveType, MemorySegment::class.java, Long::class.javaObjectType, MemorySegment::class.java),
             descriptor = FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS, ValueLayout.JAVA_LONG, ValueLayout.ADDRESS),
             invoker = object : BooleanInvoker {
-                override fun invoke(state: State, args: Array<out Any?>): Boolean = invoke(decode(args[0] as Long))
+                override fun invoke(args: Array<out Any?>): Boolean = invoke(decode(args[0] as Long))
             },
         )
     }
@@ -77,7 +74,7 @@ internal object JvmWinRtBooleanResultDelegates {
             methodType = MethodType.methodType(Int::class.javaPrimitiveType, MemorySegment::class.java, Float::class.javaObjectType, MemorySegment::class.java),
             descriptor = FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS, ValueLayout.JAVA_FLOAT, ValueLayout.ADDRESS),
             invoker = object : BooleanInvoker {
-                override fun invoke(state: State, args: Array<out Any?>): Boolean = invoke(decode(args[0] as Float))
+                override fun invoke(args: Array<out Any?>): Boolean = invoke(decode(args[0] as Float))
             },
         )
     }
@@ -88,7 +85,7 @@ internal object JvmWinRtBooleanResultDelegates {
             methodType = MethodType.methodType(Int::class.javaPrimitiveType, MemorySegment::class.java, Double::class.javaObjectType, MemorySegment::class.java),
             descriptor = FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS, ValueLayout.JAVA_DOUBLE, ValueLayout.ADDRESS),
             invoker = object : BooleanInvoker {
-                override fun invoke(state: State, args: Array<out Any?>): Boolean = invoke(decode(args[0] as Double))
+                override fun invoke(args: Array<out Any?>): Boolean = invoke(decode(args[0] as Double))
             },
         )
     }
@@ -99,7 +96,7 @@ internal object JvmWinRtBooleanResultDelegates {
             methodType = MethodType.methodType(Int::class.javaPrimitiveType, MemorySegment::class.java, MemorySegment::class.java, MemorySegment::class.java),
             descriptor = FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS, ValueLayout.ADDRESS, ValueLayout.ADDRESS),
             invoker = object : BooleanInvoker {
-                override fun invoke(state: State, args: Array<out Any?>): Boolean = invoke(decode(args[0] as MemorySegment))
+                override fun invoke(args: Array<out Any?>): Boolean = invoke(decode(args[0] as MemorySegment))
             },
         )
     }
@@ -140,123 +137,20 @@ internal object JvmWinRtBooleanResultDelegates {
         descriptor: FunctionDescriptor,
         invoker: BooleanInvoker,
     ): WinRtDelegateHandle {
-        val arena = Arena.ofShared()
-        val vtable = arena.allocate(ValueLayout.ADDRESS.byteSize() * 4, ValueLayout.ADDRESS.byteAlignment())
-        val instance = arena.allocate(ValueLayout.ADDRESS)
-        val state = State(iid, invoker)
-        states[instance.address()] = state
-        val invokeMethod = lookup.findVirtual(BooleanInvoker::class.java, "invokeRaw", booleanGenericMethodType(methodType.parameterCount() - 2))
-            .bindTo(invoker)
+        val invokeMethod = lookup.findVirtual(
+            BooleanInvoker::class.java,
+            "invokeRaw",
+            booleanGenericMethodType(methodType.parameterCount() - 2),
+        ).bindTo(invoker)
             .asType(methodType)
-        val invokeStub = linker.upcallStub(
-            invokeMethod,
-            descriptor,
-            libraryArena,
-        )
-        vtable.setAtIndex(ValueLayout.ADDRESS, 0, queryInterfaceStub)
-        vtable.setAtIndex(ValueLayout.ADDRESS, 1, addRefStub)
-        vtable.setAtIndex(ValueLayout.ADDRESS, 2, releaseStub)
-        vtable.setAtIndex(ValueLayout.ADDRESS, 3, invokeStub)
-        instance.set(ValueLayout.ADDRESS, 0, vtable)
-        return object : WinRtDelegateHandle {
-            override val pointer: ComPtr = ComPtr(AbiIntPtr(instance.address()))
-            private val closed = AtomicBoolean(false)
-
-            override fun close() {
-                if (!closed.compareAndSet(false, true)) {
-                    return
-                }
-                states.remove(instance.address())
-                arena.close()
-            }
-        }
+        val invokeStub = linker.upcallStub(invokeMethod, descriptor, libraryArena)
+        return createHandle(iid, invokeStub)
     }
 
-    private val iUnknownIid = guidOf("00000000-0000-0000-c000-000000000046")
-    private val iAgileObjectIid = guidOf("94ea2b94-e9cc-49e0-c0ff-ee64ca8f5b90")
-    private val linker: Linker = Linker.nativeLinker()
-    private val libraryArena: Arena = Arena.ofAuto()
-    private val lookup = MethodHandles.lookup()
-    private val states = ConcurrentHashMap<Long, State>()
-
-    private val queryInterfaceStub = linker.upcallStub(
-        lookup.findStatic(
-            JvmWinRtBooleanResultDelegates::class.java,
-            "queryInterface",
-            MethodType.methodType(Int::class.javaPrimitiveType, MemorySegment::class.java, MemorySegment::class.java, MemorySegment::class.java),
-        ),
-        FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS, ValueLayout.ADDRESS, ValueLayout.ADDRESS),
-        libraryArena,
-    )
-
-    private val addRefStub = linker.upcallStub(
-        lookup.findStatic(
-            JvmWinRtBooleanResultDelegates::class.java,
-            "addRef",
-            MethodType.methodType(Int::class.javaPrimitiveType, MemorySegment::class.java),
-        ),
-        FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS),
-        libraryArena,
-    )
-
-    private val releaseStub = linker.upcallStub(
-        lookup.findStatic(
-            JvmWinRtBooleanResultDelegates::class.java,
-            "release",
-            MethodType.methodType(Int::class.javaPrimitiveType, MemorySegment::class.java),
-        ),
-        FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS),
-        libraryArena,
-    )
-
-    @JvmStatic
-    private fun queryInterface(thisPointer: MemorySegment, iid: MemorySegment, result: MemorySegment): Int {
-        val state = states[thisPointer.address()] ?: return KnownHResults.E_POINTER.value
-        return if (
-            matchesGuid(iid, iUnknownIid) ||
-            matchesGuid(iid, iAgileObjectIid) ||
-            matchesGuid(iid, state.iid)
-        ) {
-            result.reinterpret(ValueLayout.ADDRESS.byteSize()).set(ValueLayout.ADDRESS, 0, thisPointer)
-            state.refCount.incrementAndGet()
-            0
-        } else {
-            result.reinterpret(ValueLayout.ADDRESS.byteSize()).set(ValueLayout.ADDRESS, 0, MemorySegment.NULL)
-            KnownHResults.E_NOINTERFACE.value
-        }
-    }
-
-    @JvmStatic
-    private fun addRef(thisPointer: MemorySegment): Int {
-        val state = states[thisPointer.address()] ?: return 1
-        return state.refCount.incrementAndGet()
-    }
-
-    @JvmStatic
-    private fun release(thisPointer: MemorySegment): Int {
-        val state = states[thisPointer.address()] ?: return 0
-        return state.refCount.decrementAndGet()
-    }
-
-    private fun matchesGuid(segment: MemorySegment, expected: Guid): Boolean {
-        val guidSegment = segment.reinterpret(16L)
-        return guidSegment.get(ValueLayout.JAVA_INT, 0L) == expected.data1 &&
-            guidSegment.get(ValueLayout.JAVA_SHORT, 4L) == expected.data2 &&
-            guidSegment.get(ValueLayout.JAVA_SHORT, 6L) == expected.data3 &&
-            expected.data4.indices.all { index ->
-                guidSegment.get(ValueLayout.JAVA_BYTE, 8L + index) == expected.data4[index]
-            }
-    }
-
-    private class State(
-        val iid: Guid,
-        val invoker: BooleanInvoker,
-    ) {
-        val refCount = AtomicInteger(1)
-    }
+    private fun isLivePointer(thisPointer: MemorySegment): Boolean = hasState(thisPointer)
 
     private interface BooleanInvoker {
-        fun invoke(state: State, args: Array<out Any?>): Boolean
+        fun invoke(args: Array<out Any?>): Boolean
 
         fun invokeRaw(thisPointer: MemorySegment, result: MemorySegment): Int = writeBooleanResult(thisPointer, result)
         fun invokeRaw(thisPointer: MemorySegment, arg0: Any?, result: MemorySegment): Int = writeBooleanResult(thisPointer, result, arg0)
@@ -269,11 +163,13 @@ internal object JvmWinRtBooleanResultDelegates {
         fun invokeRaw(thisPointer: MemorySegment, arg0: Any?, arg1: Any?, arg2: Any?, arg3: Any?, arg4: Any?, arg5: Any?, arg6: Any?, arg7: Any?, result: MemorySegment): Int = writeBooleanResult(thisPointer, result, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7)
 
         private fun writeBooleanResult(thisPointer: MemorySegment, result: MemorySegment, vararg args: Any?): Int {
-            val state = states[thisPointer.address()] ?: return KnownHResults.E_POINTER.value
-            result.reinterpret(ValueLayout.JAVA_INT.byteSize().toLong()).set(
+            if (!JvmWinRtBooleanResultDelegates.isLivePointer(thisPointer)) {
+                return KnownHResults.E_POINTER.value
+            }
+            result.reinterpret(ValueLayout.JAVA_INT.byteSize()).set(
                 ValueLayout.JAVA_INT,
                 0,
-                if (invoke(state, args)) 1 else 0,
+                if (invoke(args)) 1 else 0,
             )
             return HResult(0).value
         }
--- winrt-core/src/jvmMain/kotlin/dev/winrt/core/JvmWinRtUnitResultDelegates.kt
+++ winrt-core/src/jvmMain/kotlin/dev/winrt/core/JvmWinRtUnitResultDelegates.kt
@@ -7,25 +7,22 @@ import dev.winrt.kom.HResult
 import dev.winrt.kom.HString
 import dev.winrt.kom.KnownHResults
 import dev.winrt.kom.PlatformHStringBridge
-import java.lang.foreign.Arena
 import java.lang.foreign.FunctionDescriptor
-import java.lang.foreign.Linker
 import java.lang.foreign.MemorySegment
 import java.lang.foreign.ValueLayout
 import java.lang.invoke.MethodHandles
 import java.lang.invoke.MethodType
-import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.atomic.AtomicInteger
-import java.util.concurrent.atomic.AtomicBoolean
 
-internal object JvmWinRtUnitResultDelegates {
+internal object JvmWinRtUnitResultDelegates : JvmWinRtDelegateSupport() {
+    private val lookup = MethodHandles.lookup()
+
     fun createDelegate(
         iid: Guid,
         parameterKinds: List<WinRtDelegateValueKind>,
         invoke: (Array<Any?>) -> Unit,
     ): WinRtDelegateHandle {
         val invoker = object : UnitInvoker {
-            override fun invoke(state: State, args: Array<out Any?>) {
+            override fun invoke(args: Array<out Any?>) {
                 invoke(decodeArguments(parameterKinds, args))
             }
         }
@@ -44,7 +41,7 @@ internal object JvmWinRtUnitResultDelegates {
             methodType = MethodType.methodType(Int::class.javaPrimitiveType, MemorySegment::class.java),
             descriptor = FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS),
             invoker = object : UnitInvoker {
-                override fun invoke(state: State, args: Array<out Any?>) {
+                override fun invoke(args: Array<out Any?>) {
                     callback()
                 }
             },
@@ -57,7 +54,7 @@ internal object JvmWinRtUnitResultDelegates {
             methodType = MethodType.methodType(Int::class.javaPrimitiveType, MemorySegment::class.java, Int::class.javaObjectType),
             descriptor = FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS, ValueLayout.JAVA_INT),
             invoker = object : UnitInvoker {
-                override fun invoke(state: State, args: Array<out Any?>) {
+                override fun invoke(args: Array<out Any?>) {
                     invoke(decode(args[0] as Int))
                 }
             },
@@ -70,7 +67,7 @@ internal object JvmWinRtUnitResultDelegates {
             methodType = MethodType.methodType(Int::class.javaPrimitiveType, MemorySegment::class.java, Long::class.javaObjectType),
             descriptor = FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS, ValueLayout.JAVA_LONG),
             invoker = object : UnitInvoker {
-                override fun invoke(state: State, args: Array<out Any?>) {
+                override fun invoke(args: Array<out Any?>) {
                     invoke(decode(args[0] as Long))
                 }
             },
@@ -83,7 +80,7 @@ internal object JvmWinRtUnitResultDelegates {
             methodType = MethodType.methodType(Int::class.javaPrimitiveType, MemorySegment::class.java, Float::class.javaObjectType),
             descriptor = FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS, ValueLayout.JAVA_FLOAT),
             invoker = object : UnitInvoker {
-                override fun invoke(state: State, args: Array<out Any?>) {
+                override fun invoke(args: Array<out Any?>) {
                     invoke(decode(args[0] as Float))
                 }
             },
@@ -96,7 +93,7 @@ internal object JvmWinRtUnitResultDelegates {
             methodType = MethodType.methodType(Int::class.javaPrimitiveType, MemorySegment::class.java, Double::class.javaObjectType),
             descriptor = FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS, ValueLayout.JAVA_DOUBLE),
             invoker = object : UnitInvoker {
-                override fun invoke(state: State, args: Array<out Any?>) {
+                override fun invoke(args: Array<out Any?>) {
                     invoke(decode(args[0] as Double))
                 }
             },
@@ -109,7 +106,7 @@ internal object JvmWinRtUnitResultDelegates {
             methodType = MethodType.methodType(Int::class.javaPrimitiveType, MemorySegment::class.java, MemorySegment::class.java),
             descriptor = FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS, ValueLayout.ADDRESS),
             invoker = object : UnitInvoker {
-                override fun invoke(state: State, args: Array<out Any?>) {
+                override fun invoke(args: Array<out Any?>) {
                     invoke(decode(args[0] as MemorySegment))
                 }
             },
@@ -151,123 +148,20 @@ internal object JvmWinRtUnitResultDelegates {
         descriptor: FunctionDescriptor,
         invoker: UnitInvoker,
     ): WinRtDelegateHandle {
-        val arena = Arena.ofShared()
-        val vtable = arena.allocate(ValueLayout.ADDRESS.byteSize() * 4, ValueLayout.ADDRESS.byteAlignment())
-        val instance = arena.allocate(ValueLayout.ADDRESS)
-        val state = State(iid, invoker)
-        states[instance.address()] = state
-        val invokeMethod = lookup.findVirtual(UnitInvoker::class.java, "invokeRaw", unitGenericMethodType(methodType.parameterCount() - 1))
-            .bindTo(invoker)
+        val invokeMethod = lookup.findVirtual(
+            UnitInvoker::class.java,
+            "invokeRaw",
+            unitGenericMethodType(methodType.parameterCount() - 1),
+        ).bindTo(invoker)
             .asType(methodType)
-        val invokeStub = linker.upcallStub(
-            invokeMethod,
-            descriptor,
-            libraryArena,
-        )
-        vtable.setAtIndex(ValueLayout.ADDRESS, 0, queryInterfaceStub)
-        vtable.setAtIndex(ValueLayout.ADDRESS, 1, addRefStub)
-        vtable.setAtIndex(ValueLayout.ADDRESS, 2, releaseStub)
-        vtable.setAtIndex(ValueLayout.ADDRESS, 3, invokeStub)
-        instance.set(ValueLayout.ADDRESS, 0, vtable)
-        return object : WinRtDelegateHandle {
-            override val pointer: ComPtr = ComPtr(AbiIntPtr(instance.address()))
-            private val closed = AtomicBoolean(false)
-
-            override fun close() {
-                if (!closed.compareAndSet(false, true)) {
-                    return
-                }
-                states.remove(instance.address())
-                arena.close()
-            }
-        }
+        val invokeStub = linker.upcallStub(invokeMethod, descriptor, libraryArena)
+        return createHandle(iid, invokeStub)
     }
 
-    private val iUnknownIid = guidOf("00000000-0000-0000-c000-000000000046")
-    private val iAgileObjectIid = guidOf("94ea2b94-e9cc-49e0-c0ff-ee64ca8f5b90")
-    private val linker: Linker = Linker.nativeLinker()
-    private val libraryArena: Arena = Arena.ofAuto()
-    private val lookup = MethodHandles.lookup()
-    private val states = ConcurrentHashMap<Long, State>()
-
-    private val queryInterfaceStub = linker.upcallStub(
-        lookup.findStatic(
-            JvmWinRtUnitResultDelegates::class.java,
-            "queryInterface",
-            MethodType.methodType(Int::class.javaPrimitiveType, MemorySegment::class.java, MemorySegment::class.java, MemorySegment::class.java),
-        ),
-        FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS, ValueLayout.ADDRESS, ValueLayout.ADDRESS),
-        libraryArena,
-    )
-
-    private val addRefStub = linker.upcallStub(
-        lookup.findStatic(
-            JvmWinRtUnitResultDelegates::class.java,
-            "addRef",
-            MethodType.methodType(Int::class.javaPrimitiveType, MemorySegment::class.java),
-        ),
-        FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS),
-        libraryArena,
-    )
-
-    private val releaseStub = linker.upcallStub(
-        lookup.findStatic(
-            JvmWinRtUnitResultDelegates::class.java,
-            "release",
-            MethodType.methodType(Int::class.javaPrimitiveType, MemorySegment::class.java),
-        ),
-        FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS),
-        libraryArena,
-    )
-
-    @JvmStatic
-    private fun queryInterface(thisPointer: MemorySegment, iid: MemorySegment, result: MemorySegment): Int {
-        val state = states[thisPointer.address()] ?: return KnownHResults.E_POINTER.value
-        return if (
-            matchesGuid(iid, iUnknownIid) ||
-            matchesGuid(iid, iAgileObjectIid) ||
-            matchesGuid(iid, state.iid)
-        ) {
-            result.reinterpret(ValueLayout.ADDRESS.byteSize()).set(ValueLayout.ADDRESS, 0, thisPointer)
-            state.refCount.incrementAndGet()
-            0
-        } else {
-            result.reinterpret(ValueLayout.ADDRESS.byteSize()).set(ValueLayout.ADDRESS, 0, MemorySegment.NULL)
-            KnownHResults.E_NOINTERFACE.value
-        }
-    }
-
-    @JvmStatic
-    private fun addRef(thisPointer: MemorySegment): Int {
-        val state = states[thisPointer.address()] ?: return 1
-        return state.refCount.incrementAndGet()
-    }
-
-    @JvmStatic
-    private fun release(thisPointer: MemorySegment): Int {
-        val state = states[thisPointer.address()] ?: return 0
-        return state.refCount.decrementAndGet()
-    }
-
-    private fun matchesGuid(segment: MemorySegment, expected: Guid): Boolean {
-        val guidSegment = segment.reinterpret(16L)
-        return guidSegment.get(ValueLayout.JAVA_INT, 0L) == expected.data1 &&
-            guidSegment.get(ValueLayout.JAVA_SHORT, 4L) == expected.data2 &&
-            guidSegment.get(ValueLayout.JAVA_SHORT, 6L) == expected.data3 &&
-            expected.data4.indices.all { index ->
-                guidSegment.get(ValueLayout.JAVA_BYTE, 8L + index) == expected.data4[index]
-            }
-    }
-
-    private class State(
-        val iid: Guid,
-        val invoker: UnitInvoker,
-    ) {
-        val refCount = AtomicInteger(1)
-    }
+    private fun isLivePointer(thisPointer: MemorySegment): Boolean = hasState(thisPointer)
 
     private interface UnitInvoker {
-        fun invoke(state: State, args: Array<out Any?>)
+        fun invoke(args: Array<out Any?>)
 
         fun invokeRaw(thisPointer: MemorySegment): Int = invokeWithResult(thisPointer)
         fun invokeRaw(thisPointer: MemorySegment, arg0: Any?): Int = invokeWithResult(thisPointer, arg0)
@@ -280,8 +174,10 @@ internal object JvmWinRtUnitResultDelegates {
         fun invokeRaw(thisPointer: MemorySegment, arg0: Any?, arg1: Any?, arg2: Any?, arg3: Any?, arg4: Any?, arg5: Any?, arg6: Any?, arg7: Any?): Int = invokeWithResult(thisPointer, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7)
 
         private fun invokeWithResult(thisPointer: MemorySegment, vararg args: Any?): Int {
-            val state = states[thisPointer.address()] ?: return KnownHResults.E_POINTER.value
-            invoke(state, args)
+            if (!JvmWinRtUnitResultDelegates.isLivePointer(thisPointer)) {
+                return KnownHResults.E_POINTER.value
+            }
+            invoke(args)
             return HResult(0).value
         }
     }
